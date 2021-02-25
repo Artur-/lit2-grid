328af28140e793a1602ebad6a0b9e25ed798d7bb
Artur Signell <artur@vaadin.com>
2021-02-25 22:19:52 +0200

    Generated project

---
+++ frontend/views/empty/empty-view.ts
@@ -0,0 +1,10 @@
+import '!style-loader!css-loader!./empty-view.css';
+import { customElement, html } from 'lit-element';
+import { View } from '../../views/view';
+
+@customElement('empty-view')
+export class EmptyView extends View {
+  render() {
+    return html`<div>Content placeholder</div>`;
+  }
+}
